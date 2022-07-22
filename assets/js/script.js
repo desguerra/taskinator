56a53f667990f864551f595f63fca0b7c54c319c
desguerra <ddesguerra@ucdavis.edu>
2022-07-22 12:52:47 -0700

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
+// target button
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+// target the #tasks-to-do ul element
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// important to place this function BEFORE the event listener!!
+var createTaskHandler = function() {
+    // create new list element
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    listItemEl.textContent = "This is a new task.";
+
+    // append li element to page inside the correct ul element
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
