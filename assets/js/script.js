5a97d4de291b45d56b67154bafe56a993d469040
desguerra <ddesguerra@ucdavis.edu>
2022-07-22 17:33:09 -0700

    create initial form and change color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-// target button
-var buttonEl = document.querySelector("#save-task");
+// target the form id, to listen to the entire form
+// rather than just the button
+var formEl = document.querySelector("#task-form");
 // target the #tasks-to-do ul element
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // important to place this function BEFORE the event listener!!
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     // create new list element
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
     listItemEl.textContent = "This is a new task.";
-
     // append li element to page inside the correct ul element
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
